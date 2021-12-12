6120952fcc1e7802241b07fe84f29e88618f64fb
Kacper Stolarek <59137941+Capure@users.noreply.github.com>
2021-12-12 13:22:02 +0100

    :sparkles: Removed the Carousel component

--- OnlineJudgeFE/src/pages/oj/views/general/Home.vue
+++ OnlineJudgeFE/src/pages/oj/views/general/Home.vue
@@ -1,48 +1,36 @@
 <template>
   <Flex justify="around" style="width: 100%">
     <div>
-      <panel shadow v-if="contests.length" class="contest">
+      <panel
+        shadow
+        v-for="(contest, index) of contests"
+        :key="index"
+        class="contest"
+      >
         <div slot="title">
-          <Button type="text" class="contest-title" @click="goContest">{{
-            contests[index].title
-          }}</Button>
+          <Button
+            type="text"
+            class="contest-title"
+            @click="() => goContest(index)"
+            >{{ contest.title }}</Button
+          >
         </div>
-        <Carousel
-          v-model="index"
-          trigger="hover"
-          autoplay
-          :autoplay-speed="6000"
-          class="contest"
-        >
-          <CarouselItem v-for="(contest, index) of contests" :key="index">
-            <div class="contest-content">
-              <div class="contest-content-tags">
-                <Button type="info" shape="circle" size="small" icon="calendar">
-                  {{ contest.start_time | localtime('YYYY-M-D HH:mm') }}
-                </Button>
-                <Button
-                  type="success"
-                  shape="circle"
-                  size="small"
-                  icon="android-time"
-                >
-                  {{ getDuration(contest.start_time, contest.end_time) }}
-                </Button>
-                <Button
-                  type="warning"
-                  shape="circle"
-                  size="small"
-                  icon="trophy"
-                >
-                  {{ contest.rule_type }}
-                </Button>
-              </div>
-              <div class="contest-content-description">
-                <blockquote v-html="contest.description"></blockquote>
-              </div>
-            </div>
-          </CarouselItem>
-        </Carousel>
+        <div class="contest-content">
+          <div class="contest-content-tags">
+            <Button type="info" shape="circle" size="small" icon="calendar">
+              {{ contest.start_time | localtime('YYYY-M-D HH:mm') }}
+            </Button>
+            <Button type="success" shape="circle" size="small" icon="clock">
+              {{ getDuration(contest.start_time, contest.end_time) }}
+            </Button>
+            <Button type="warning" shape="circle" size="small" icon="trophy">
+              {{ contest.rule_type }}
+            </Button>
+          </div>
+          <div class="contest-content-description">
+            <blockquote v-html="contest.description"></blockquote>
+          </div>
+        </div>
       </panel>
       <Announcements class="announcement"></Announcements>
     </div>
@@ -66,8 +54,7 @@ export default {
   },
   data () {
     return {
-      contests: [],
-      index: 0
+      contests: []
     }
   },
   mounted () {
@@ -80,10 +67,10 @@ export default {
     getDuration (startTime, endTime) {
       return time.duration(startTime, endTime)
     },
-    goContest () {
+    goContest (index) {
       this.$router.push({
         name: 'contest-details',
-        params: { contestID: this.contests[this.index].id }
+        params: { contestID: this.contests[index].id }
       })
     }
   }
@@ -92,6 +79,7 @@ export default {
 
 <style lang="less" scoped>
 .contest {
+  margin-bottom: 10px;
   &-title {
     font-style: italic;
     font-size: 21px;
