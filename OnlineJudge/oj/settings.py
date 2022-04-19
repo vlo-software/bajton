9eb9b2917f4852c295ecbce7d7a860bcef2a1fb1
Kacper Stolarek <stolarek83@gmail.com>
2022-04-19 18:53:04 +0200

    :bug: Removed the development leftovers

--- OnlineJudge/oj/settings.py
+++ OnlineJudge/oj/settings.py
@@ -61,7 +61,7 @@ INSTALLED_APPS = VENDOR_APPS + LOCAL_APPS
 MIDDLEWARE = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    # 'django.middleware.csrf.CsrfViewMiddleware', # TODO: enable this
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'account.middleware.APITokenAuthMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
