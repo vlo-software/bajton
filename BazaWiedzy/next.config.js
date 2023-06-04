b3c7d2b623689a81bb5b26b68082180a7ba9901b
Kacper Stolarek <59137941+Capure@users.noreply.github.com>
2023-06-04 19:18:00 +0200

    Fixes hot reload on windows (#36)
    * :bug: Fixed hot reload on windows
    
    * :see_no_evil: Removed random comment

--- BazaWiedzy/next.config.js
+++ BazaWiedzy/next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: "/bw",
+  webpack: (config, { dev }) => {
+    if (!dev) return config;
+    config.watchOptions = {
+      poll: 1000, // Check for changes every second
+      aggregateTimeout: 300, // delay before rebuilding
+    };
+    return config;
+  },
 };
 
 module.exports = nextConfig;
