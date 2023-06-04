9fbc61c4435ba8fe8f362b860eef3a482704b3ec
Kacper Stolarek <59137941+Capure@users.noreply.github.com>
2023-06-04 19:14:03 +0200

    :see_no_evil: Removed random comment

--- BazaWiedzy/next.config.js
+++ BazaWiedzy/next.config.js
@@ -3,7 +3,6 @@ const nextConfig = {
   basePath: "/bw",
   webpack: (config, { dev }) => {
     if (!dev) return config;
-    // Solve compiling problem via vagrant
     config.watchOptions = {
       poll: 1000, // Check for changes every second
       aggregateTimeout: 300, // delay before rebuilding
