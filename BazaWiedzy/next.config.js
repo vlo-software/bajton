ccb8c90ea7bde4f685d3ff90e44d1f2b7ad2bb5b
Kacper Stolarek <stolarek83@gmail.com>
2023-06-04 19:07:43 +0200

    :bug: Fixed hot reload on windows

--- BazaWiedzy/next.config.js
+++ BazaWiedzy/next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: "/bw",
+  webpack: (config, { dev }) => {
+    if (!dev) return config;
+    // Solve compiling problem via vagrant
+    config.watchOptions = {
+      poll: 1000, // Check for changes every second
+      aggregateTimeout: 300, // delay before rebuilding
+    };
+    return config;
+  },
 };
 
 module.exports = nextConfig;
